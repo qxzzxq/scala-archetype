f35dc189b3016c345bade6160b58a164a0e65bdb
Xuzhou Qin <me@qinx.dev>
2020-05-25 23:10:28 +0200

    init

---
+++ src/main/resources/archetype-resources/src/main/scala/HelloWorld.scala
@@ -0,0 +1,18 @@
+package ${
+package
+
+}
+
+/**
+ * @author ${user.name}
+ */
+object HelloWorld {
+
+  def foo(x: Array[String]) = x.foldLeft("")((a, b) => a + b)
+
+  def main(args: Array[String]) {
+    println("Hello World!")
+    println("concat arguments = " + foo(args))
+  }
+
+}
