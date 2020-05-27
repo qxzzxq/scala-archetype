2331832f73dec928e6d93162d92d373a8ae5e850
Xuzhou Qin <me@qinx.dev>
2020-05-27 10:21:11 +0200

    fix: SimpleTestSuite test: add integration test

--- src/main/resources/archetype-resources/src/test/scala/SimpleTestSuite.scala
+++ src/main/resources/archetype-resources/src/test/scala/SimpleTestSuite.scala
@@ -1,10 +1,9 @@
-package samples
-
 import org.scalatest.funsuite.AnyFunSuite
 
 class SimpleTestSuite extends AnyFunSuite {
 
   test("This should pass") {
+    println("hello")
     assert(true)
   }
 
